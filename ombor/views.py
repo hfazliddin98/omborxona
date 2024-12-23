85a388e81be77d9214501782f3c9fc37b998f14b
hfazliddin98 <hatamovfazliddin505@gmail.com>
2024-12-23 09:02:39 +0500

    23.12.2024

--- ombor/views.py
+++ ombor/views.py
@@ -10,6 +10,7 @@ from rest_framework import generics
 from rest_framework import views
 from rest_framework import filters
 from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import ModelViewSet
 from .models import Kategoriya, Maxsulot, Birlik, OmborniYopish, Ombor, Korzinka
 from .models import OlinganMaxsulotlar, Buyurtma, JamiMahsulot, Talabnoma, RadEtilganMaxsulotlar
@@ -22,47 +23,69 @@ from .serializers import JamiMahsulotSerializer, TalabnomaSerializer, RadEtilgan
 class KategoriyaViewSet(ModelViewSet):
     queryset = Kategoriya.objects.all()
     serializer_class = KategoriyaSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['name']
 
 class MaxsulotViewSet(ModelViewSet):
     queryset = Maxsulot.objects.all()
     serializer_class = MaxsulotSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['kategoriya', 'name', 'maxviylik', 'it_park']
 
 class BirlikViewSet(ModelViewSet):
     queryset = Birlik.objects.all()
     serializer_class = BirlikSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['name']
 
 class OmborniYopishViewSet(ModelViewSet):
     queryset = OmborniYopish.objects.all()
     serializer_class = OmborniYopishSerializer
 
+
 class OmborViewSet(ModelViewSet):
     queryset = Ombor.objects.all()
     serializer_class = OmborSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['maxsulot', 'qiymat', 'birlik']
 
 
 class BuyurtmaViewSet(ModelViewSet):
     queryset = Buyurtma.objects.all()
     serializer_class = BuyurtmaSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['buyurtma']
+    filterset_fields = ['user', 'active', 'sorov', 'rad']
 
 class KorzinkaViewSet(ModelViewSet):
     queryset = Korzinka.objects.all()
     serializer_class = KorzinkaSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['buyurtma', 'maxsulot', 'qiymat', 'birlik', 'active']
 
 class OlinganMaxsulotlarViewSet(ModelViewSet):
     queryset = OlinganMaxsulotlar.objects.all()
     serializer_class = OlinganMaxsulotlarSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['buyurtma', 'maxsulot', 'qiymat', 'birlik', 'active']
 
 class RadEtilganMaxsulotlarViewSet(ModelViewSet):
     queryset = RadEtilganMaxsulotlar.objects.all()
     serializer_class = RadEtilganMaxsulotlarSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['user', 'buyurtma', 'maxsulot', 'qiymat', 'birlik', 'active']
 
 class JamiMahsulotViewSet(ModelViewSet):
     queryset = JamiMahsulot.objects.all()
     serializer_class = JamiMahsulotSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['maxsulot', 'qiymat', 'birlik']
 
 class TalabnomaViewSet(ModelViewSet):
     queryset = Talabnoma.objects.all()
     serializer_class = TalabnomaSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['buyurtma']
 
 
 
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import ModelViewSet
 from .models import Users, Binos
 from .serializers import UserSerializer, BinosSerializer
@@ -14,9 +15,17 @@ def bosh_sahifa(request):
 class UserViewSet(ModelViewSet):
     queryset = Users.objects.filter(is_superuser=False)
     serializer_class = UserSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = [
+        'name','bino','superadmin',
+        'prorektor','bugalter','xojalik_bolimi',
+        'it_park','omborchi','komendant'
+        ]
 
 
 class BinosViewSet(ModelViewSet):
     queryset = Binos.objects.all()
     serializer_class = BinosSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['name']
 
