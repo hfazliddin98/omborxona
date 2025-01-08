fd4e0fbce4e938ba53f4f40d1dd8ca15d2ed781c
hfazliddin98 <hatamovfazliddin505@gmail.com>
2025-01-08 08:42:40 +0500

    8.1.2025

--- users/serializers.py
+++ users/serializers.py
@@ -16,7 +16,7 @@ class UserGetSerializer(serializers.ModelSerializer):
     bino = BinosSerializer()
     class Meta:
         model = Users
-        fields = ['id', 'username', 'first_name', 'last_name', 'parol', 'lavozim', 'bino', 'role', 'is_active', 'created_at']
+        fields = ['id', 'username', 'first_name', 'last_name', 'parol', 'lavozim', 'bino', 'role', 'is_active']
     
 
 class UserPostSerializer(serializers.ModelSerializer):
