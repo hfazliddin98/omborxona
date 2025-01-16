e52d5595fcc475597ea4bfbba5d9728e016dde8b
hfazliddin98 <hatamovfazliddin505@gmail.com>
2025-01-16 09:05:45 +0500

    16.1.2025

--- ombor/urls.py
+++ ombor/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from rest_framework.routers import SimpleRouter
-from .views import BuyurtmaMaxsulotViewSet
 from .views import KategoriyaViewSet, MaxsulotViewSet, BirlikViewSet, OmborniYopishViewSet, OmborViewSet
 from .views import OlinganMaxsulotViewSet, BuyurtmaViewSet
 from .views import RadEtilganMaxsulotlarViewSet, TalabnomaViewSet
@@ -14,7 +13,6 @@ router.register(r'birlik', BirlikViewSet, basename='birlik')
 router.register(r'omborni_yopish', OmborniYopishViewSet, basename='omborni_yopish')
 router.register(r'ombor', OmborViewSet, basename='ombor')
 router.register(r'buyurtma', BuyurtmaViewSet, basename='buyurtma')
-router.register(r'buyurtma_maxsulot', BuyurtmaMaxsulotViewSet, basename='buyurtma_maxsulot')
 router.register(r'olingan_maxsulotlar', OlinganMaxsulotViewSet, basename='olingan_maxsulotlar')
 router.register(r'rad_etilgan_maxsulotlar', RadEtilganMaxsulotlarViewSet, basename='rad_etilgan_maxsulotlar')
 router.register(r'talabnoma', TalabnomaViewSet, basename='talabnoma')
